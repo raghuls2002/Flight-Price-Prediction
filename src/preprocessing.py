cdc0d3b7ec58d2771952194f3979f7bdaf16a452
raghuls2002 <raghul.sureshkumar@gmail.com>
2023-06-25 21:55:08 +0530

    Create preprocessing.py

---
+++ src/preprocessing.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 25 17:26:22 2023
+
+@author: user
+"""
+
+import pandas as pd 
+
+# Loading dataset
+data = pd.read_csv("../data/dataset.csv")
+data = data.drop(['Unnamed: 0','flight'], axis=1)
+
+target = 'price'
+cols = data.columns.tolist()
+cols.remove(target)
+categorical_cols= data.select_dtypes(include=['object']).columns.tolist()
+
+# Label Encoding
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+
+label_map = {}
+
+for column in categorical_cols:
+    le.fit(data[column])
+    data[column] = le.transform(data[column])
+    
+    label_map[column] = dict(zip(le.classes_, le.transform(le.classes_)))
+    
+print(label_map)
+
+# Splitting into X and y
+y = data[target]
+X = data.drop(columns=[target], axis = 1)
+
+# Scaling independent variables
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+X_s = scaler.fit_transform(X)
+X = pd.DataFrame(X_s, columns = X.columns)
+
+
+# Splitting into training and testing data
+from sklearn.model_selection import train_test_split
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+
+
+
