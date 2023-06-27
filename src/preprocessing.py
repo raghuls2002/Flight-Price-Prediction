05d787f5b566c345aba0faa74fa5f302b891325b
raghuls2002 <raghul.sureshkumar@gmail.com>
2023-06-27 19:53:04 +0530

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -12,8 +12,6 @@ data = pd.read_csv("../data/dataset.csv")
 data = data.drop(['Unnamed: 0','flight'], axis=1)
 
 target = 'price'
-cols = data.columns.tolist()
-cols.remove(target)
 categorical_cols= data.select_dtypes(include=['object']).columns.tolist()
 
 # Label Encoding
@@ -35,19 +33,21 @@ print(label_map)
 y = data[target]
 X = data.drop(columns=[target], axis = 1)
 
-# Scaling independent variables
-from sklearn.preprocessing import StandardScaler
-
-scaler = StandardScaler()
-
-X_s = scaler.fit_transform(X)
-X = pd.DataFrame(X_s, columns = X.columns)
-
 
 # Splitting into training and testing data
 from sklearn.model_selection import train_test_split
 
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+
+
+# Scaling independent variables
+from sklearn.preprocessing import StandardScaler
+
+scaler = StandardScaler()
+
+scaler.fit(X_train)
+X_train_s = scaler.transform(X_train)
+X_test_s = scaler.transform(X_test)
 
 
 
